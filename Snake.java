b2d2e045b8cb6e0d3b31f0f479989622afe76b59
w1nner4444 <johnnybyrnes@yahoo.com>
2017-05-11 10:48:21 -0700

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -3,7 +3,7 @@ public class Snake
     String direction;
     int length = 4;
     Board board;
-    int size;
+    int player;
 
     Snake(int x, int y, int len, boolean player2)
     {
@@ -14,6 +14,7 @@ public class Snake
             board.set(3,y/2,"body");
             board.set(2,y/2,"tail");
             direction = "right";
+            player = 2;
         }
         else
         {
@@ -22,6 +23,7 @@ public class Snake
             board.set(x-3,y/2,"body");
             board.set(x-2,y/2,"tail");
             direction = "left";
+            player = 1;
         }
     }
 
@@ -31,60 +33,62 @@ public class Snake
         switch(direction)
         {
             case "down":
-            if ((board.get(x, y-1)).equals("body"))
+            if ((board.getVal(x, y-1)).equals("body"))
             {
                 return false;
             }
             return true;
-            break;
             case "up":
-            if ((board.get(x, y+1)).equals("body"))
+            if ((board.getVal(x, y+1)).equals("body"))
             {
                 return false;
             }
             return true;
-            break;
             case "left":
-            if ((board.get(x-1, y)).equals("body"))
+            if ((board.getVal(x-1, y)).equals("body"))
             {
                 return false;
             }
             return true;
-            break;
             case "right":
-            if ((board.get(x+1, y)).equals("body"))
+            if ((board.getVal(x+1, y)).equals("body"))
             {
                 return false;
             }
             return true;
-            break;
         }
+        return true;
     }
 
     // actually move the snake head, remove tail
-    public void move(int x, int y)
+    public void move()
     {
         // move head if allowed
+        int x, y;
+        int[] head = board.getPos("head");
+        x = head[0];
+        y = head[1];
         boolean legal = this.checkMove(x, y);
         if (legal)
         {
             switch(direction)
             {
                 case "down":
+                board.getVal(x, y);
                 board.set(x, y, "body");
-                board.set(x, y-1, "head");
+                board.set(x, y-1, "down head");
                 break;
                 case "up":
                 board.set(x, y, "body");
-                board.set(x, y-1, "head");
+                board.set(x, y-1, "up head");
                 break;
                 case "right":
                 board.set(x, y, "body");
-                board.set(x+1, y, "head");
+                board.set(x+1, y, "right head");
                 break;
                 case "left":
                 board.set(x, y, "body");
-                board.set(x-1, y, "head");
+                board.set(x-1, y, "left head");
                 break;
             }
         }
@@ -92,18 +96,12 @@ public class Snake
         {
             this.lose();
         }
+        
         //remove tail
-        for (int x = 0; x < size; x++)
-        {
-            for (int p: arr)
-            {
-                if (
-            }
-        }
+        
     }
 
     public void lose()
     {
-
     }
 }
